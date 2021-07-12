3f2d2b4ba5efffa5d966087223263cc3fb916612
jranders92 <87340905+jranders92@users.noreply.github.com>
2021-07-12 17:46:49 -0400

    Update PortfolioProjectQuery_v.2.sql

--- PortfolioProjectQuery_v.2.sql
+++ PortfolioProjectQuery_v.2.sql
@@ -1,18 +1,18 @@
 
 --Selecting Data from all Tables
 Select * from PortfolioProject.dbo.CovidDeaths
-Select * from PortfolioProject.dbo.CovidVaccinations
+Select * from PortfolioProject.dbo.CovidVaccinations;
 
 --Covid Death Info from United States
 Select * from PortfolioProject.dbo.CovidDeaths
-where location = 'United States'
+where location = 'United States';
 
 --Population of Each Location (in alphabetical order).
 Select location, MAX(population) AS Population
 from PortfolioProject.dbo.CovidDeaths
 Where continent IS NOT NULL
 Group by location
-Order by location
+Order by location;
 
 --Global Population Death Rate Using Subquery and CTE. Used CAST to convert column from varchar to integer.
 With GlobalPopulation_CTE
@@ -23,7 +23,7 @@ Group By location)
 
 Select SUM(LocationPopulation) As GlobalPopulation, SUM(LocationDeaths) As GlobalDeaths, 
 (Select Sum(LocationDeaths)/Sum(LocationPopulation) AS GlobalDeathRate from GlobalPopulation_CTE) As GlobalDeathRate
-from GlobalPopulation_CTE
+from GlobalPopulation_CTE;
 
 --Death Rate by Location Using a JOIN and CTE.
 WITH DeathRate_CTE (location, Population, TotalDeaths)
@@ -35,7 +35,7 @@ Group by location)
 
 Select *, (TotalDeaths/Population)as DeathRate
 From DeathRate_CTE
-Order by DeathRate DESC
+Order by DeathRate DESC;
 
 --Full Vaccination Rate by Location using a JOIN and CTE. 
 With VaccinationRate_CTE (location, population, PeopleFullyVaccinated)
@@ -51,9 +51,9 @@ Select *, (PeopleFullyVaccinated/population) as FullVaccinationRate
 From VaccinationRate_CTE
 Where PeopleFullyVaccinated IS NOT NULL
 AND population IS NOT NULL
-Order by FullVaccinationRate DESC
+Order by FullVaccinationRate DESC;
 
 -- Location Total Deaths by Day using PARTITION BY
 Select location, date, population, new_deaths, SUM(cast(new_deaths as INT)) OVER (Partition by location order by location, date) AS TotalDeathsByDate
 from PortfolioProject.[dbo].[CovidDeaths]
-Where continent IS NOT NULL
+Where continent IS NOT NULL;
